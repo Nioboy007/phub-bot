9f27226fdd6d4e7266473478a0312604f73dde44
petalscarp <160005317+petalscarp@users.noreply.github.com>
2024-02-14 14:56:47 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -2,7 +2,7 @@ from pyrogram.errors.exceptions import MessageNotModified, FloodWait
 import asyncio
 import threading
 
-def humanbytes(size):
+def human_bytes(size):
     if not size:
         return ""
     power = 2 ** 10
@@ -12,7 +12,6 @@ def humanbytes(size):
         size /= power
         raised_to_pow += 1
     return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
-
 
 def edit_msg(client, message, to_edit):
     try:
@@ -24,14 +23,13 @@ def edit_msg(client, message, to_edit):
     except TypeError:
         pass
 
-
 def download_progress_hook(d, message, client):
     if d['status'] == 'downloading':
-        current = d.get("_downloaded_bytes_str") or humanbytes(int(d.get("downloaded_bytes", 1)))
+        current = d.get("_downloaded_bytes_str") or human_bytes(int(d.get("downloaded_bytes", 1)))
         total = d.get("_total_bytes_str") or d.get("_total_bytes_estimate_str")
         file_name = d.get("filename")
         eta = d.get('_eta_str', "N/A")
         percent = d.get("_percent_str", "N/A")
         speed = d.get("_speed_str", "N/A")
-        to_edit = f"<b><u>Mengunduh File</b></u> \n<b>Nama File :</b> <code>{file_name}</code> \n<b>Ukuran File :</b> <code>{total}</code> \n<b>Keceptan Mengunduh :</b> <code>{speed}</code> \n<b>ETA :</b> <code>{eta}</code> \n<i>Mengunduh {current} dari {total}</i> (__{percent}__)"
+        to_edit = f"<b><u>Downloading File</u></b>\n<b>File Name:</b> <code>{file_name}</code>\n<b>File Size:</b> <code>{total}</code>\n<b>Download Speed:</b> <code>{speed}</code>\n<b>ETA:</b> <code>{eta}</code>\n<i>Downloading {current} of {total}</i> (<i>{percent}</i>)"
         threading.Thread(target=edit_msg, args=(client, message, to_edit)).start()
